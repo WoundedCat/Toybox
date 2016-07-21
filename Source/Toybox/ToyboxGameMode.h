eeccfd9b062052decc1cfd56e1f7317c58bcd591
GyungHaeMin <evolvingmin@live.co.kr>
2016-07-22 02:43:15 +0900

    Add Source put in the Source Controls

---
+++ Source/Toybox/ToyboxGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ToyboxGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOYBOX_API AToyboxGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
